369a059ad6e774f0772dabd6941cee786f1cfff9
SwanX1 <karliscern@gmail.com>
2021-06-26 17:40:11 +0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,149 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import { formatWithOptions } from 'util';
+
+export type PrefixPredicate = (level: LoggerLevel) => string;
+export type FilterPredicate = (data: string | Uint8Array, ansiFreeData: string | Uint8Array) => boolean;
+
+export interface LoggerOutput {
+  stream: { write: NodeJS.WritableStream['write'] };
+  level?: LoggerLevel;
+  prefix?: PrefixPredicate;
+  filter?: FilterPredicate;
+}
+
+export interface LoggerOptions {
+  streams: LoggerOutput[];
+}
+
+export enum LoggerLevel {
+  DEBUG = 0,
+  INFO  = 1,
+  WARN  = 2,
+  ERROR = 3,
+  FATAL = 4,
+}
+
+export function getLoggerLevelName(level: LoggerLevel): 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL' {
+  if (level == LoggerLevel.DEBUG || level < 0) return 'DEBUG';
+  if (level == LoggerLevel.INFO) return 'INFO';
+  if (level == LoggerLevel.WARN) return 'WARN';
+  if (level == LoggerLevel.ERROR) return 'ERROR';
+  if (level == LoggerLevel.FATAL || level > 4) return 'FATAL';
+  return 'INFO';
+}
+
+export function coloredLog(level: LoggerLevel): string {
+  const date: Date = new Date();
+  const time = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+  let loglevelcolor: string;
+  switch (level) {
+    case LoggerLevel.DEBUG:
+      loglevelcolor = 'blue';
+      break;
+    case LoggerLevel.INFO:
+      loglevelcolor = 'green';
+      break;
+    case LoggerLevel.WARN:
+      loglevelcolor = 'yellow';
+      break;
+    case LoggerLevel.ERROR:
+      loglevelcolor = 'red';
+      break;
+    case LoggerLevel.FATAL:
+      loglevelcolor = 'red';
+      break;
+  }
+  let loggerLevelPrefix: string = getLoggerLevelName(level);
+  loggerLevelPrefix = `[${loggerLevelPrefix}]`.padEnd(7, ' ');
+  return chalk`{gray [${time}]} {${loglevelcolor} ${loggerLevelPrefix}} `;
+}
+
+export class Logger {
+  static defaultOptions: LoggerOptions = {
+    streams: [
+      {
+        stream: process.stdout,
+        level: LoggerLevel.INFO,
+        prefix: (): string => `[${new Date().toLocaleTimeString()}] `
+      }
+    ],
+  };
+
+  private options: LoggerOptions;
+  private outputs: LoggerOutput[];
+
+  constructor(options?: LoggerOptions) {
+    this.options = options ?? Logger.defaultOptions;
+    this.outputs = [];
+    for (const output of this.options.streams) {
+      this.addOutput(output);
+    }
+  }
+
+  public addOutput(output: LoggerOutput): this {
+    this.outputs.push(output);
+    return this;
+  }
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  // HACKY STUFF, DO NOT TOUCH
+  private static getFormattedForStdout(callback: (formatted: string) => unknown, ...data: any[]): void {
+    new console.Console({
+      write: callback,
+    } as NodeJS.WritableStream).log(...data);
+  }
+
+  public log(...data: any[]): void {
+    this.internalLog(LoggerLevel.INFO, ...data);
+  }
+
+  public debug(...data: any[]): void {
+    this.internalLog(LoggerLevel.DEBUG, ...data);
+  }
+  
+  public info(...data: any[]): void {
+    this.internalLog(LoggerLevel.INFO, ...data);
+  }
+  
+  public warn(...data: any[]): void {
+    this.internalLog(LoggerLevel.WARN, ...data);
+  }
+  
+  public error(...data: any[]): void {
+    this.internalLog(LoggerLevel.ERROR, ...data);
+  }
+  
+  public fatal(...data: any[]): void {
+    this.internalLog(LoggerLevel.FATAL, ...data);
+  }
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private internalLog(level: LoggerLevel, ...data: any[]): void {
+    for (const output of this.outputs) {
+      if ((output.level ?? LoggerLevel.INFO) <= level) {
+        let s = '';
+        if (typeof output.prefix === 'function') {
+          s += output.prefix(level);
+        }
+
+        s += formatWithOptions.apply(this, [{ colors: true }, ...data]);
+        // Remove ansi codes
+        let sStripped = s.replace(
+          // eslint-disable-next-line no-control-regex
+          /[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))/g,
+          ''
+          );
+        if (typeof output.filter === 'function' && !output.filter(s, sStripped)) continue;
+        s += '\n';
+        sStripped += '\n';
+        if (output.stream instanceof fs.WriteStream) {
+          output.stream.write(sStripped);
+        } else {
+          output.stream.write(s);
+        }
+      }
+    }
+  }
+}
---
+++ test/index.test.js
@@ -0,0 +1,76 @@
+/// <reference types="jest" />
+const { Logger, coloredLog, getLoggerLevelName, LoggerLevel } = require('../dist/index');
+const { WriteStream } = require('fs');
+const chalk = require('chalk');
+
+test('logger level is in correct order', () => {
+  expect(LoggerLevel.DEBUG).toBeLessThan(LoggerLevel.INFO);
+  expect(LoggerLevel.INFO).toBeLessThan(LoggerLevel.WARN);
+  expect(LoggerLevel.WARN).toBeLessThan(LoggerLevel.ERROR);
+  expect(LoggerLevel.ERROR).toBeLessThan(LoggerLevel.FATAL);
+});
+
+test('coloredLog outputs correct string', () => {
+  const regex = (color, level) => new RegExp(`^\x1B\\[90m\\[\\d{2}:\\d{2}:\\d{2}\\]\x1B\\[39m \x1B\\${color}${('\\[' + level + '\\]').padEnd(9, ' ')}\x1B\\[39m $`);
+
+  expect(coloredLog(LoggerLevel.DEBUG)).toMatch(regex('[34m', 'DEBUG'));
+  expect(coloredLog(LoggerLevel.INFO)).toMatch(regex('[32m', 'INFO'));
+  expect(coloredLog(LoggerLevel.WARN)).toMatch(regex('[33m', 'WARN'));
+  expect(coloredLog(LoggerLevel.ERROR)).toMatch(regex('[31m', 'ERROR'));
+  expect(coloredLog(LoggerLevel.FATAL)).toMatch(regex('[31m', 'FATAL'));
+});
+
+test('getLoggerLevelName returns correct string', () => {
+  const levels = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'];
+  for (const level of levels) {
+    expect(getLoggerLevelName(LoggerLevel[level])).toBe(level);
+  }
+});
+
+test('respects loggerlevels', () => {
+  const levels = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'];
+
+  for (const level of levels) {
+    let wrote = false;
+    const write = () => wrote = true;
+    const logger = new Logger({
+      streams: [
+        {
+          level: LoggerLevel[level],
+          stream: { write }
+        }
+      ]
+    });
+
+    for (const comparelevel of levels) {
+      logger[comparelevel.toLowerCase()]();
+      expect(wrote).toBe(LoggerLevel[level] <= LoggerLevel[comparelevel]);
+      wrote = false;
+    }
+  }
+});
+
+test('strips formatting for fs.WriteStream', () => {
+  class MockWriteStream extends WriteStream {
+    constructor(writeFunc) {
+      super('');
+      this.write = writeFunc;
+    }
+
+    open() {}
+  }
+
+  let writtenString = '';
+  const logger = new Logger({
+    streams: [
+      {
+        stream: new MockWriteStream(s => writtenString = s)
+      }
+    ]
+  });
+
+  logger.info('test');
+  expect(writtenString).toBe('test\n');
+  logger.info(chalk`{yellow Hello} World!`);
+  expect(writtenString).toBe('Hello World!\n');
+});
