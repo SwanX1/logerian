bd96fea060ee6283a321de78624923fccad2bba2
SwanX1 <karliscern@gmail.com>
2022-01-07 18:53:35 +0200

    docs: Added JSDocs and README

---
+++ README.md
@@ -0,0 +1,84 @@
+<h1 align="center">Logerian</h1>
+
+### Description
+Logerian is a logging utility made by [me](https://github.com/SwanX1)!
+I made this logging utility mainly because I am too dumb to understand other loggers like [winston](https://www.npmjs.com/package/winston) and [signale](https://www.npmjs.com/package/signale).
+If you're willing to contribute, please read [CONTRIBUTING.md](./CONTRIBUTING.md).
+
+### Usage
+To use this logger, you can simply just create a new logger instance and it'll work:
+```typescript
+import { Logger } from "logerian"; // ES import
+const { Logger } = require("logerian"); // CJS import
+
+const logger = new Logger();
+
+logger.log("Hello World!");
+// Output: [17:43:01] Hello World!
+```
+
+By default, the logger creates a stream with a timestamp as shown in the example code above.
+If you wish to change that, you'll have to define a stream when you create the logger.
+```typescript
+const logger = new Logger({
+  streams: [
+    {
+      stream: process.stdout,
+    },
+  ],
+});
+
+logger.log("foobar");
+// Output: foobar
+```
+
+A logger can utilize multiple output streams:
+```typescript
+const logger = new Logger({
+  streams: [
+    {
+      stream: process.stdout,
+    },
+    {
+      stream: fs.createWriteStream("log.txt"),
+    },
+  ],
+});
+
+logger.log("Iron Man dies in Endgame");
+// Output: Iron Man dies in Endgame
+logger.log("Steve Rogers is old!!");
+// Output: Steve Rogers is old!!
+```
+```conf
+# log.txt
+Iron Man dies in Endgame
+Steve Rogers is old!!
+```
+
+There's also a neat thing called log levels!
+```typescript
+import { Logger, LoggerLevel } from "logerian";
+const { Logger, LoggerLevel } = require("logerian");
+
+const logger = new Logger({
+  streams: [
+    {
+      level: LoggerLevel.WARN,
+      stream: process.stdout,
+    },
+    {
+      level: LoggerLevel.DEBUG, // Debug level is default
+      stream: fs.createWriteStream("log.txt"),
+    }
+  ],
+});
+
+// Logs to both - stdout and log.txt
+logger.error("Uh oh! There's an error!");
+
+// Logs only to log.txt
+logger.debug("By the way, there's an error because your code sucks!");
+```
+
+For further information, read the JSDocs.
--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,46 @@ export type PrefixPredicate = (level: LoggerLevel, identifier?: string | symbol)
 export type FilterPredicate = (data: string | Uint8Array, ansiFreeData: string | Uint8Array) => boolean;
 
 export interface LoggerOutput {
+  /**
+   * Stream that will be used to output the log messages.
+   * If the stream is a {@link Logger} instance, the output will be logged to the stream. without filtering and without a prefix.
+   */
   stream: Pick<NodeJS.WritableStream, 'write'> | Logger;
+  /**
+   * Determines the verbosity of the output.
+   * If the stream is a {@link Logger} instance, this value will be used.
+   */
   level?: LoggerLevel;
+  /**
+   * The prefix predicate that will be used to format the log message.
+   * If the stream is a {@link Logger} instance, this value will not be used.
+   */
   prefix?: PrefixPredicate;
+  /**
+   * This predicate will be used to filter log messages. If the function returns true, the message will be logged.
+   * If the stream is a {@link Logger} instance, this value will not be used.
+   */
   filter?: FilterPredicate;
 }
 
 export interface LoggerOptions {
+  /**
+   * Used for heirarchical logging. The identifier will be passed on to the identifier prefix function.
+   */
   identifier: string | symbol;
+  /**
+   * Used for heirarchical logging. This determines the prefix added to the log message before passing it on to the parent logger.
+   */
   identifierPrefix?: PrefixPredicate;
+  /**
+   * Determines the outputs that will be used to log the messages. Duplicate streams will be filtered out.
+   */
   streams: LoggerOutput[];
 }
 
+/**
+ * LoggerLevels are used to determine the verbosity of logger outputs.
+ */
 export enum LoggerLevel {
   DEBUG = 0,
   INFO  = 1,
@@ -25,6 +53,9 @@ export enum LoggerLevel {
   FATAL = 4,
 }
 
+/**
+ * Converts a LoggerLevel to a string.
+ */
 export function getLoggerLevelName(level: LoggerLevel): 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL' {
   if (level == LoggerLevel.DEBUG || level < 0) return 'DEBUG';
   if (level == LoggerLevel.INFO) return 'INFO';
@@ -34,6 +65,11 @@ export function getLoggerLevelName(level: LoggerLevel): 'DEBUG' | 'INFO' | 'WARN
   return 'INFO';
 }
 
+/**
+ * Returns a colored identifier prefix predicate.
+ * @param identifierColor The color to use for the identifier. Accepts [ANSI escape codes](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors).
+ * @param bracketColor The color to use for the brackets. Accepts [ANSI escape codes](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors).
+ */
 export function coloredIdentifier(identifierColor = 0, bracketColor = 0): PrefixPredicate {
   return (_level: LoggerLevel, identifier?: string | symbol): string => {
     if (identifier == null) return '';
@@ -44,6 +80,9 @@ export function coloredIdentifier(identifierColor = 0, bracketColor = 0): Prefix
   }
 }
 
+/**
+ * Returns a colored prefix string.
+ */
 export function coloredLog(level: LoggerLevel): string {
   const date: Date = new Date();
   let loglevelcolor: number;
@@ -94,10 +133,16 @@ export class Logger {
     }
   }
 
+  /**
+   * @default Symbol('Unnamed')
+   */
   public get identifier(): string | symbol {
     return this.options.identifier;
   }
 
+  /**
+   * Adds a new output stream to the logger. If the exact same output stream is already added, it will be ignored.
+   */
   public addOutput(output: LoggerOutput): this {
     if (!this.outputs.some(o => o.stream === output.stream || ('write' in o.stream && 'write' in output.stream && o.stream.write === output.stream.write))) {
       this.outputs.push(output);
@@ -105,37 +150,63 @@ export class Logger {
     return this;
   }
 
+  /**
+   * Removes an output stream from the logger.
+   */
   public removeOutput(outputStream: LoggerOutput['stream']): this {
     this.outputs = this.outputs.filter(output => output.stream !== outputStream);
     return this;
   }
 
   /* eslint-disable @typescript-eslint/no-explicit-any */
+  /**
+   * Shorthand for {@link Logger.info()}
+   */
   public log(...data: any[]): void {
     this.internalLog(LoggerLevel.INFO, ...data);
   }
 
+  /**
+   * Logs a message with log level {@link LoggerLevel.DEBUG}
+   */
   public debug(...data: any[]): void {
     this.internalLog(LoggerLevel.DEBUG, ...data);
   }
 
+  /**
+   * Logs a message with log level {@link LoggerLevel.INFO}
+   */
   public info(...data: any[]): void {
     this.internalLog(LoggerLevel.INFO, ...data);
   }
 
+  /**
+   * Logs a message with log level {@link LoggerLevel.WARN}
+   */
   public warn(...data: any[]): void {
     this.internalLog(LoggerLevel.WARN, ...data);
   }
 
+  /**
+   * Logs a message with log level {@link LoggerLevel.ERROR}
+   */
   public error(...data: any[]): void {
     this.internalLog(LoggerLevel.ERROR, ...data);
   }
 
+  /**
+   * Logs a message with log level {@link LoggerLevel.FATAL}
+   */
   public fatal(...data: any[]): void {
     this.internalLog(LoggerLevel.FATAL, ...data);
   }
   /* eslint-enable @typescript-eslint/no-explicit-any */
 
+  /**
+   * This is an internal method that logs a message with the given log level.
+   * It will only log the message if the log level is greater than or equal to the level of the output.
+   * @private
+   */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   protected internalLog(level: LoggerLevel, ...data: any[]): void {
     for (const output of this.outputs) {
