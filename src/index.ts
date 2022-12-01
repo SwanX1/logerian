cf49b4388467cb535a4802d673fc958e7907eb0c
Kārlis Čerņavskis <karliscern@gmail.com>
2022-12-01 19:49:23 +0200

    !cleanup: Refactored exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export { addLoggerLevel, LoggerLevel, LoggerLevelData } from './level';
-export { Logger, LoggerOptions, LoggerStream } from './logger';
-export { coloredLog, stripANSIFormatting } from './util';
+import { addLoggerLevel, LoggerLevel, LoggerLevelData } from './level';
+import { Logger, LoggerOptions, LoggerStream } from './logger';
+import { PinnedLine } from './pinnedline';
+import { coloredLog, stripANSIFormatting } from './util';
+
+const util = {
+  addLoggerLevel,
+  coloredLog,
+  stripANSIFormatting,
+};
+
+export { Logger, LoggerOptions, LoggerStream, LoggerLevel, LoggerLevelData, PinnedLine, util };
